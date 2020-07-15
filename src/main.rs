46e2183ef658bb25f1b5ee739609d9cf6cdf701b
br1code <brunogiovagnoli@gmail.com>
2020-07-14 21:07:56 -0300

    fix some comments + remove println

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 use std::io;
 use std::cmp::Ordering;
-use rand::Rng;
-// The Rng trait defines methods that random number generators implement, and this
+use rand::Rng; // The Rng trait defines methods that random number generators implement, and this
 // trait must be in scope for us to use those methods.
 
 fn main() {
     println!("Guess the number!");
 
     // The rand::thread_rng function will give us the particular random number generator
-    // that we’re going to use: one that is local to the current thread of execution
-    // and seeded by the operating system.
+    // that we’re going to use: one that is local to the current thread of execution and seeded by the operating system.
     // Then we call the gen_range method on the random number generator
     // This method is defined by the Rng trait that we brought into scope with the use rand::Rng statement.
     let secret_number = rand::thread_rng().gen_range(1, 101);
@@ -25,20 +23,17 @@ fn main() {
 
         // stdin() creates a handler to the standard input of the current process.
         // read_line() takes one string argument and it stores
-        // the value entered by the user and stores it to given argument
-        // this argument variable must be mutable, and also we add the & keyword to
-        // make it works a reference value.
-        // References are immutable by default, that's why we
-        // write &mut guess rather than &guess to make it mutable.
-        // read_line() returns a Result type, Results are like Enums
-        // for Result, te variants are Ok or Err.
-        // we use the expect function (from Result types) to do something depending on the result value.
+        // the value entered by the user and stores it to given argument.
+        // This argument variable must be mutable, and also we add the & keyword to make it works a reference value.
+        // References are immutable by default, that's why we write &mut guess rather than &guess to make it mutable.
+        // read_line() returns a Result type, Results are like Enums. For Result, the variants are Ok or Err.
+        // We use the expect function (from Result types) to do something depending on the result value.
         // If this instance of io::Result is an Err value, expect will cause the program to
         // crash and display the message that you passed as an argument to expect.
         // If this instance of io::Result is an Ok value, expect will take the return value that Ok
         // is holding and return just that value to you so you can use it. In this case, that value
         // is the number of bytes in what the user entered into standard input.
-        // note: we are not storing the return value from read_line
+        // Note: we are not storing the return value from read_line
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
@@ -66,8 +61,6 @@ fn main() {
             Ok(num) => num,
             Err(_) => continue // _ is a catchall value, we want to match all Err values
         };
-
-        println!("You guessed: {}", guess);
 
         // The cmp method compares two values and can be called on anything that can be compared
         // It takes a reference to whatever you want to compare with:
